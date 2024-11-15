4e966ebbb41dc45288a55ba2d8053559a5356bb7
CristiannMaidana <cristian.mg.david@gmail.com>
2024-11-14 23:34:04 -0300

    En el archivo models.py, se cambio el nombre a la tabla Usuarios por Vecinos, y se le agrego el autentificador de inicio de sesion de Django.

--- RecoleccionDeResiduos/EcoUshuaia/models.py
+++ RecoleccionDeResiduos/EcoUshuaia/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Calendarios(models.Model):
@@ -52,14 +53,15 @@ class Sensores(models.Model):
     id_contenedor = models.ForeignKey(Contenedores, models.DO_NOTHING, db_column='id_contenedor')
 
 
-class Usuarios(models.Model):
-    id_usuarios = models.AutoField(primary_key=True)
+class Vecinos(models.Model):
+    id_vecino = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=35)
     apellido = models.CharField(max_length=35)
     usuario = models.CharField(max_length=35)
     dni = models.IntegerField(unique=True)
     email = models.CharField(unique=True, max_length=40)
     id_zona = models.ForeignKey('Zonas', models.DO_NOTHING, db_column='id_zona', blank=True, null=True)
+    id_usuario = models.OneToOneField(User, models.DO_NOTHING, db_column='id', null=True)
 
 
 class Zonas(models.Model):
